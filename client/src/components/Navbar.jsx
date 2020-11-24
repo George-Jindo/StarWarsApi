d6a210f8cbb9f5e4c330d2ba95106ceebcec7f45
George J <g.jindo@gmail.com>
2020-11-24 03:48:22 -0700

    Created nav and bg image

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className='logo'>
+                <i className='fab fa-jedi-order'></i>
+                <h4>Star Wars Api</h4>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
